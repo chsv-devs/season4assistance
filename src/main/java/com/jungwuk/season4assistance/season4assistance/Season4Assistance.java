56876cb6179a5e4b3672511b41823a306571027f
Hancho1577 <vojougae35@gmail.com>
2021-07-22 01:26:59 +0700

    클래스명 변경

--- src/main/java/com/jungwuk/season4assistance/season4assistance/Season4Assistance.java
+++ src/main/java/com/jungwuk/season4assistance/season4assistance/Season4Assistance.java
@@ -21,7 +21,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Season4assistance extends JavaPlugin implements Listener {
+public final class Season4Assistance extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
