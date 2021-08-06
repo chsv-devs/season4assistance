1fb8438aa58b687e6a167cbe9532e759ac9d05af
Hancho1577 <vojougae35@gmail.com>
2021-08-06 21:19:21 +0700

    몹 레벨을 캐시합니다

--- src/main/java/com/jungwuk/season4assistance/HUtils.java
+++ src/main/java/com/jungwuk/season4assistance/HUtils.java
@@ -4,27 +4,37 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.LinkedHashMap;
 import java.util.logging.Level;
 
 public class HUtils {
+    public static LinkedHashMap<String, Integer> cachedMobLevel = new LinkedHashMap<>();
 
     // 몹 레벨을 이름에서 가져옵니다.
     public static int parseMobLevel(String entityName) {
+        Integer cachedLevel = cachedMobLevel.get(entityName);
+        if (cachedLevel != null) {
+            return cachedLevel;
+        }
+
+        int level;
         try {
             if (entityName.contains("[")) {
                 String filtered = ChatColor.stripColor(entityName).split("\\[")[1].split("레벨")[0].trim();
                 if (filtered.contains("]")) {
-                    return -1;
+                    level = -1;
+                } else {
+                    level = Integer.parseInt(filtered);
                 }
-
-                return Integer.parseInt(filtered);
             } else {
-                return -2;
+                level = -2;
             }
         } catch (Exception e) {
             Bukkit.getLogger().log(Level.WARNING, "몹 레벨을 가져오지 못했습니다.", e);
+            level = -3;
         }
-        return -3;
+        cachedMobLevel.put(entityName, level);
+        return level;
     }
 
     // JE 유저인지 확인합니다.
